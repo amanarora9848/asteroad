c4085d6fa2f39fd36d1d65b088bb0ca5db1ac828
amanarora9848 <aman.arora9848@gmail.com>
2019-04-07 02:43:31 +0530

    Final main.dart commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,89 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+import 'dart:math';
 void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title:'AsteRoad',
+      home: MyHomePage(title: 'AsteRoad'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class MyHomePage extends StatefulWidget{
+  MyHomePage({Key key, this.title}):super(key:key);
   final String title;
-
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState()=>_MyHomePageState();
 }
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin{
+  final String url="https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY";
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  void initState(){super.initState(); this.getJsonData();}
+  Future<List> getJsonData() async{
+    var rng=new Random();
+    int l;
+    for(var i=0;i<12;i++){l=rng.nextInt(5);}
+    var response = await http.get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+    var init=json.decode(response.body)["near_earth_objects"][l];
+    var name=init["name"];
+    var diameter=init["estimated_diameter"]["kilometers"]["estimated_diameter_max"].toString();
+    var hazard=init["is_potentially_hazardous_asteroid"].toString();
+    var date=init["close_approach_data"][0]["close_approach_date"];
+    List<String> data=[name, diameter, hazard, date];
+    return data;
+  }
+  double _height;
+  double _width;
+  @override
+  Widget build(BuildContext context){
+    var height=MediaQuery.of(context).size.height, width=MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar:AppBar(title:Text(widget.title, style:TextStyle(color:Colors.white, fontWeight:FontWeight.w600, letterSpacing:2.5,),),
+        centerTitle:true,
+        backgroundColor:Colors.black,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body:Stack(
+        fit:StackFit.expand,
+        children:<Widget>[
+          Image(image:AssetImage('assets/images/asteroids.jpg'), fit:BoxFit.cover, color:Colors.black54, colorBlendMode:BlendMode.darken,),
+          Column(
+            mainAxisAlignment:MainAxisAlignment.center,
+            children: <Widget>[
+              AnimatedSize(
+                curve:Curves.fastOutSlowIn,
+                child:Container(width:_width, height:_height, child:Image(image:AssetImage('assets/images/nasa.png',),),), vsync:this, duration:Duration(seconds: 3),),
+              Container(padding:EdgeInsets.all(2.0), height: height/8, width: width/1.12, margin:EdgeInsets.all(1.0), child:Text("Welcome to AsteRoad. Get some useful information about asteroids nearby/approaching earth.", style:TextStyle(color: Colors.white, fontSize: 14), textAlign:TextAlign.center,),),
+              Container(
+                height: height/8,
+                width: width/1.12,
+                child: FutureBuilder(
+                  future: getJsonData(),
+                  builder: (BuildContext context, AsyncSnapshot snapshot){
+                    if(snapshot.data == null){return Container(child:Center(child:CircularProgressIndicator(strokeWidth: 4.0,),),);}
+                    else{return Container(
+                        height:height / 3,
+                        width:width / 1.12,
+                        margin:EdgeInsets.all(1.0),
+                        child:Column(
+                          mainAxisAlignment:MainAxisAlignment.center,
+                          children:<Widget>[
+                            Text('Asteroid name: ${snapshot.data[0]}', style:TextStyle(color:Colors.white, fontSize:14.5),), Text('Diameter: ${snapshot.data[1]} km', style:TextStyle(color:Colors.white, fontSize:14.5),), Text('Hazardous?: ${snapshot.data[2]}', style:TextStyle(color:Colors.white, fontSize:14.5),), Text('Closest Approach Date: ${snapshot.data[3]}', style:TextStyle(color:Colors.white, fontSize:14.5),),
+                          ],
+                        ),
+                      );
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
